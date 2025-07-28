9d974602fbc5caa69b9a9d451471a45e91263f63
Kellian Youbissi <Mandelaubc@gmail.com>
2025-07-28 19:17:34 -0400

    Update User entity

--- userservice/src/main/java/com/fitness/userservice/model/User.java
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -1,8 +1,9 @@
 package com.fitness.userservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.lang.annotation.Target;
 import java.time.LocalDateTime;
@@ -11,13 +12,25 @@ import java.time.LocalDateTime;
 @Table(name="users")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
     private String firstName;
     private String lastName;
+
+    @Enumerated(EnumType.STRING)
     private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
     private LocalDateTime createAt;
+
+    @UpdateTimestamp
     private LocalDateTime updateAt;
 
 }
