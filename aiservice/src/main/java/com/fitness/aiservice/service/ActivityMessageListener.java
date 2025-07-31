c4d0eaf91259d052b743e8a1ff7a67a97813fa64
Kellian Youbissi <Mandelaubc@gmail.com>
2025-07-31 19:49:13 -0400

    Set up RabbitMQ consumer in AI service

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -0,0 +1,14 @@
+package com.fitness.aiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+    public void processActivity(Activity activity) {
+
+    }
+}
